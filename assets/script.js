fa295b0642d97905f5612cbcd73e31321a1932b6
VaIlkya <laeyka13@gmail.com>
2024-10-19 16:28:57 +0200

    Add HTML and JS basic file

---
+++ assets/script.js
@@ -0,0 +1,31 @@
+console.log("Script lancée :D")
+const myForm = document.querySelector("form");
+console.log("la constante myForm contient : ", myForm);
+myForm.addEventListener("submit", function(evenement) {
+    evenement.preventDefault();
+    
+   
+    const inputFirstname = document.querySelector("#first-name").value;
+    console.log("la constante inputFirstname contient : ", inputFirstname);
+    const inputLastname = document.querySelector("#last-name").value;
+    console.log("la constante inputLastname contient : ", inputLastname);
+    const inputMessage = document.querySelector("#message").value;
+    console.log("la constante inputMessage contient : ", inputMessage);
+    if (inputFirstname == "" || inputLastname == "" || inputMessage =="") {
+     
+        document.querySelector("#error-message").style.display = "block";
+        return;
+    }
+
+    console.log("Le code continue");
+    const messageToClone = document.querySelector("#message_to_clone");
+    console.log("La constante messageToClone contient : ", messageToClone);
+    const messageCloned = messageToClone.cloneNode(true);
+    console.log("La constante messageCloned contient : ", messageCloned);
+    messageCloned.querySelector("message-max").textContent = inputFirstname + " " + inputLastname
+
+   
+});
+console.log("Le code s'est arrêté");
+
+
